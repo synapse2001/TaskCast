457c483afc0324046a491390d5a22da9b94cabb6
Gagan Agarwal <73771646+synapse2001@users.noreply.github.com>
2023-07-30 22:13:53 +0530

    Update Test.jsx optimized

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,191 +1,138 @@
-import React, { useState,useEffect } from "react";
-import "../styles/Test.css"
+import React, { useState, useEffect } from "react";
 import { auth, useFirebase } from "../context/firebase";
 import { signOut } from "firebase/auth";
 import HomePage from "./HomePage";
-// import "./Test.js"
+import "../styles/Test.css"; // Import your CSS file with theme styles
 
-let savedTheme = localStorage.getItem('savedTheme');
-const changeTheme =(color) => {
+const TestPage = ({ user, podcastData }) => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [savedTheme, setSavedTheme] = useState(localStorage.getItem('savedTheme') || 'standard');
+
+  const firebase = useFirebase();
+
+  useEffect(() => {
+    document.body.className = savedTheme;
+    // Load todos from local storage
+    const savedTodos = JSON.parse(localStorage.getItem('todos'));
+    if (savedTodos) {
+      setTodos(savedTodos);
+    }
+  }, [savedTheme]);
+
+  const changeTheme = (color) => {
     localStorage.setItem('savedTheme', color);
-    savedTheme = localStorage.getItem('savedTheme');
+    setSavedTheme(color);
+  };
 
-    document.body.className = color;
-    // Change blinking cursor for darker theme:
-    color === 'darker' ? 
-        document.getElementById('title').classList.add('darker-title')
-        : document.getElementById('title').classList.remove('darker-title');
-
-    document.querySelector('input').className = `${color}-input`;
-    // Change todo color without changing their status (completed or not):
-    document.querySelectorAll('.todo').forEach(todo => {
-        Array.from(todo.classList).some(item => item === 'completed') ? 
-            todo.className = `todo ${color}-todo completed`
-            : todo.className = `todo ${color}-todo`;
-    });
-    // Change buttons color according to their type (todo, check or delete):
-    document.querySelectorAll('button').forEach(button => {
-        Array.from(button.classList).some(item => {
-            if (item === 'check-btn') {
-              button.className = `check-btn ${color}-button`;  
-            } else if (item === 'delete-btn') {
-                button.className = `delete-btn ${color}-button`; 
-            } else if (item === 'todo-btn') {
-                button.className = `todo-btn ${color}-button`;
-            }
-        });
-    });
-}
-
-
-function savelocal(todo){
-    //Check: if item/s are there;
-    let todos;
-    if(localStorage.getItem('todos') === null) {
-        todos = [];
-    }
-    else {
-        todos = JSON.parse(localStorage.getItem('todos'));
+  const addToDo = () => {
+    if (task.trim() === '') {
+      alert("You must write something!");
+      return;
     }
 
-    todos.push(todo);
-    localStorage.setItem('todos', JSON.stringify(todos));
-}
+    const newTodo = {
+      task: task,
+      completed: false,
+    };
 
-const addToDo = (task) =>  {
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+    setTask("");
 
+    // Save todos to local storage
+    localStorage.setItem('todos', JSON.stringify([...todos, newTodo]));
+  };
 
-    // Prevents form from submitting / Prevents form from relaoding;
-    // event.preventDefault();
-    const toDoInput = document.querySelector('.todo-input');
-    const toDoList = document.querySelector('.todo-list');
-    // toDo DIV;
-    const toDoDiv = document.createElement("div");
-    toDoDiv.classList.add('todo', `${savedTheme}-todo`);
+  const toggleTodoStatus = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
 
-    // Create LI
-    const newToDo = document.createElement('li');
-    if (task === '') {
-            alert("You must write something!");
-        } 
-    else {
-        // alert("HIIIIIOIdggjd")
-        // newToDo.innerText = "hey";
-        newToDo.innerText = task;
-        newToDo.classList.add('todo-item');
-        toDoDiv.appendChild(newToDo);
+    setTodos(updatedTodos);
 
-        // Adding to local storage;
-        savelocal(task);
+    // Save updated todos to local storage
+    localStorage.setItem('todos', JSON.stringify(updatedTodos));
+  };
 
-        // check btn;
-        const checked = document.createElement('button');
-        checked.innerHTML = '<i class="fas fa-check"></i>';
-        checked.classList.add('check-btn', `${savedTheme}-button`);
-        toDoDiv.appendChild(checked);
-        // delete btn;
-        const deleted = document.createElement('button');
-        deleted.innerHTML = '<i class="fas fa-trash"></i>';
-        deleted.classList.add('delete-btn', `${savedTheme}-button`);
-        toDoDiv.appendChild(deleted);
+  const deleteToDo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
 
-        // Append to list;
-        toDoList.appendChild(toDoDiv);
+    setTodos(updatedTodos);
 
-        // CLearing the input;
-        task = '';
-    }
+    // Save updated todos to local storage
+    localStorage.setItem('todos', JSON.stringify(updatedTodos));
+  };
 
-}   
+  return (
+    <html>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#062e3f" />
+        <meta name="Description" content="A dynamic and aesthetic To-Do List WebApp." />
 
+        {/* Google Font: Quick Sand */}
+        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300&display=swap" rel="stylesheet" />
 
+        {/* font awesome (https://fontawesome.com) for basic icons; source: https://cdnjs.com/libraries/font-awesome */}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" integrity="sha256-46r060N2LrChLLb5zowXQ72/iKKNiw/lAmygmHExk/o=" crossorigin="anonymous" />
 
-
-
-const TestPage = ({user,podcastData}) => {
-
-
-    const [task, setTask] = useState("");
-    const firebase = useFirebase();
-    useEffect(() => {
-        changeTheme("standard");
-      }, []);
-    
-
-    return (
-      <html>
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name="theme-color" content="#062e3f" />
-          <meta name="Description" content="A dynamic and aesthetic To-Do List WebApp." />
-  
-          {/* Google Font: Quick Sand */}
-          <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300&display=swap" rel="stylesheet" />
-  
-          {/* font awesome (https://fontawesome.com) for basic icons; source: https://cdnjs.com/libraries/font-awesome */}
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" integrity="sha256-46r060N2LrChLLb5zowXQ72/iKKNiw/lAmygmHExk/o=" crossorigin="anonymous" />
-  
-          <link rel="shortcut icon" type="image/png" href="assets/favicon.png" />
-          {/* <link rel="stylesheet" href="/styles/Test.css" /> */}
-          <title>JUST DO IT</title>
-        </head>
-        <body className="testing">
-        <div id = "header">
-                <div class="flexrow-container">
-                    <div onClick={() => changeTheme('standard')} class="standard-theme theme-selector"></div>
-                    <div onClick={() => changeTheme('light')} class="light-theme theme-selector"></div>
-                    <div onClick={() => changeTheme('darker')} class="darker-theme theme-selector"></div>
-                </div>
-                <h1 id="title">Just do it.<div id="border"></div></h1>
-            </div>
+        <link rel="shortcut icon" type="image/png" href="assets/favicon.png" />
+        <title>JUST DO IT</title>
+      </head>
+      <body className="testing">
+        <div id="header">
+          <div className="flexrow-container">
+            <div onClick={() => changeTheme('standard')} className="standard-theme theme-selector"></div>
+            <div onClick={() => changeTheme('light')} className="light-theme theme-selector"></div>
+            <div onClick={() => changeTheme('darker')} className="darker-theme theme-selector"></div>
+          </div>
+          <h1 id="title">Just do it.<div id="border"></div></h1>
+        </div>
 
         <div className="taskinput" id="form">
-                
-                    <input onChange={e => setTask(e.target.value)} class="todo-input" value = {task} type="text" placeholder="Add a task."></input>
-                    <button onClick={()=> addToDo(task)} class="todo-btn">I Got This!</button>
-                    <button onClick={() => {signOut(auth); window.location.href = "/"}}>Logout</button>
-                
+          <input
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
+            type="text"
+            className={`todo-input ${savedTheme}-input`}
+            placeholder="Add a task."
+          />
+          <button onClick={addToDo} className={`todo-btn ${savedTheme}-button`}>
+            I Got This!
+          </button>
+          <button onClick={() => { signOut(auth); window.location.href = "/"; }}>
+            Logout
+          </button>
+        </div>
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <div key={index} className={`todo ${savedTheme}-todo ${todo.completed ? 'completed' : ''}`}>
+              <li className="todo-item">{todo.task}</li>
+              <button
+                className={`check-btn ${savedTheme}-button`}
+                onClick={() => toggleTodoStatus(index)}
+              >
+                <i className="fas fa-check" />
+              </button>
+              <button
+                className={`delete-btn ${savedTheme}-button`}
+                onClick={() => deleteToDo(index)}
+              >
+                <i className="fas fa-trash" />
+              </button>
             </div>
-                <div class="version">
-                                <div class="demo version-section"><a href="https://github.com/tusharnankani/ToDoList" class="github-corner">
-                                    {/* <svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; left: 0; transform: scale(-1, 1);"> */}
-                                    <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
-                                    {/* <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path> */}
-                                    <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
-                                    {/* </svg> */}
-                                    </a>
-                </div>
-            </div>
-            <div>
-                <p><span id="datetime"></span></p>
-                <script src="JS/time.js"></script>
-            </div>
-
-        <div id="myUnOrdList">
-                <ul class="todo-list">
-                    {/* <!-- (Basic Format)
-                    <div class="todo">
-                        items added to this list:
-                        <li></li>
-                        <button>delete</button>
-                        <button>check</button>
-                    </div> --> */}
-                </ul>
-            </div>
-            <div>
-                <button onClick={() => signOut(auth)}></button>
-            </div>
-            <div className="playercss">
-                <HomePage 
-                    user={user}
-                    podcastData={podcastData}
-                />
-            </div>
-            {/* <script src="/Test.js" type="text/javascript"> </script> */}
-        </body>
-      </html>
-    );
-  };
+          ))}
+        </ul>
+        <div className="playercss">
+          <HomePage
+            user={user}
+            podcastData={podcastData}
+          />
+        </div>
+      </body>
+    </html>
+  );
+};
 
 export default TestPage;
